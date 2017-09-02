1261731c9192f1e6a0ba5e14fb76fe6dad717cb8
RVLopes <ramonvlopes@hotmail.com>
2017-09-02 11:18:03 -0300

    criacao de roteador

--- front/src/index.js
+++ front/src/index.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 import './index.css'
 import Landing from './pages/Landing'
 import registerServiceWorker from './registerServiceWorker'
 
 import 'semantic-ui-css/semantic.min.css'
 
-ReactDOM.render(<Landing />, document.getElementById('root'))
+ReactDOM.render(
+  <Router>
+    <Route exact path="/" component={Landing}/>
+  </Router>
+  , document.getElementById('root')
+)
 registerServiceWorker()
