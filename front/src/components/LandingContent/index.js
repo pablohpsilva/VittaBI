308b8def077c87361c9d465ff7181cdbdde4c0e2
RVLopes <ramonvlopes@hotmail.com>
2017-09-02 13:41:41 -0300

    correcao de layout da landing

--- front/src/components/LandingContent/index.js
+++ front/src/components/LandingContent/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './style.css'
 import CardsGroup from '../LandingCardsGroup'
 import {
@@ -12,29 +12,48 @@ import {
 
 const FAMILY_IMG = require('./img/landing-img.png')
 
-const Content = () => (
-  <div
-    className="landing-container"
-  >
-    <Image
-      className="landing-image"
-      floated='left'
-      size='big'
-      src={FAMILY_IMG}
-    />
-    <CardsGroup />
-    <Link to="/application">
-      <Button
-        basic
-        className="landing-header-button-enter"
-        size="massive"
-        color='blue'
-      >
-        Conheça a plataforma
-      </Button>
-    </Link>
-  </div>
-)
+class Content extends Component {
 
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      windowWidth: 0,
+    }
+  }
+
+  componentDidMount() {
+    window.onresize = () => {
+      this.setState({ windowWidth: window.innerWidth })
+    }
+  }
+
+  render() {
+    return (
+      <div
+        className="landing-container"
+      >
+        <Image
+          hidden={this.state.windowWidth > 648}
+          className="landing-image"
+          floated='left'
+          size='big'
+          src={FAMILY_IMG}
+        />
+        <CardsGroup />
+        <Link to="/application">
+          <Button
+            basic
+            className="landing-header-button-enter"
+            size="massive"
+            color='blue'
+          >
+            Conheça a plataforma
+          </Button>
+        </Link>
+      </div>
+    )
+  }
+}
 
 export default Content
