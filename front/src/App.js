daf23be8b6bc3c1b5a852ad8f306c25f2623bb7a
pablohpsilva <pablohpsilva@gmail.com>
2017-09-02 10:31:26 -0300

    criacao da apliacao

--- front/src/App.js
+++ front/src/App.js
@@ -1,37 +1,10 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import 'semantic-ui-css/semantic.min.css';
+import Application from './components/Application'
 
-import Chart from './components/CardChart'
-
-const data = [
-  { name: 'Page A', uv: 4000, pv: 2400, amt: 2400 },
-  { name: 'Page B', uv: 3000, pv: 1398, amt: 2210 },
-  { name: 'Page C', uv: 2000, pv: 9800, amt: 2290 },
-  { name: 'Page D', uv: 2780, pv: 3908, amt: 2000 },
-  { name: 'Page E', uv: 1890, pv: 4800, amt: 2181 },
-  { name: 'Page F', uv: 2390, pv: 3800, amt: 2500 },
-  { name: 'Page G', uv: 3490, pv: 4300, amt: 2100 },
-]
-
-class App extends Component {
+export default class Index extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Chart
-          data={data}
-        />
-      </div>
+      <Application/>
     );
   }
 }
-
-export default App;
