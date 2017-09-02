b8464efe8d2b900ef14ff93ec8dd8952ac15cb40
pablohpsilva <pablohpsilva@gmail.com>
2017-09-02 15:01:38 -0300

    correcoes de layout

--- front/src/components/LandingHeader/index.js
+++ front/src/components/LandingHeader/index.js
@@ -8,26 +8,31 @@ import {
 const LOGO_VITTA = require('./img/logo-vitta.png')
 
 const Header = (props) => (
-  <div className="landing-header">
-    <Image
-      centered
-      src={LOGO_VITTA}
-      size="medium"
-    />
-    <h1
-      className="landing-header-title"
-    >
-      Vitta auxiliando o crescimento da informação,<br/>contribuindo para a saúde do Brasil.
-    </h1>
-    <Divider />
-    <Button
-      inverted
-      size="massive"
-      className="landing-header-button"
-      onClick={props.handleScrollPage}
-    >
-      + Novidades
+  <div
+    className="landing-header"
+    style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}>
+    <div>
+      <Image
+        centered
+        src={LOGO_VITTA}
+        size="medium"
+      />
+      <h1 className="landing-header-title">
+        Vitta auxiliando o crescimento da informação,<br />contribuindo para a saúde do Brasil.
+      </h1>
+      <Divider />
+      <Button
+        inverted
+        size="massive"
+        className="landing-header-button"
+        onClick={props.handleScrollPage}>
+        + Novidades
     </Button>
+    </div>
   </div>
 )
 
